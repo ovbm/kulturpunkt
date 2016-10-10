fe7f5a4f7dc966ce39348798a0d360175ba11693
Olivier Baumann <olivier@ovbm.co>
2016-10-10 13:18:38 +0200

    fixed toolbar bug

--- web/themes/iovi/js/main.js
+++ web/themes/iovi/js/main.js
@@ -1,4 +1,4 @@
-/* Toggle between adding and removing the "active" and "show" classes when the user clicks on one of the "Section" buttons. The "active" class is used to add a background color to the current button when its belonging panel is open. The "show" class is used to open the specific accordion panel */
+//Toggle between adding and removing the "active" and "show" classes when the user clicks on one of the "Section" buttons. The "active" class is used to add a background color to the current button when its belonging panel is open. The "show" class is used to open the specific accordion panel
 var acc = document.getElementsByClassName("accordion");
 var i;
 
