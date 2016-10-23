104f152de0db88df321738a606241c4bd376f9c0
drupal <drupal@d3659c5e113e>
2016-10-23 18:27:39 +0000

    reinstalled google maps

--- web/sites/default/aliases.drushrc.php
+++ web/sites/default/aliases.drushrc.php
@@ -1,7 +1,19 @@
 <?php
-// - And we load the aliases file here
+// Don't change anything here, it's magic!
 global $aliases_stub;
 if (empty($aliases_stub)) {
-  $aliases_stub = file_get_contents('https://raw.githubusercontent.com/AmazeeLabs/devops/master/drush-deployment/aliases.drushrc.php.stub?' . rand(0, 99999999999));
+  $aliases_stub = file_get_contents('https://raw.githubusercontent.com/amazeeio/drush-aliases/master/aliases.drushrc.php.stub?' . rand(0, 99999999999));
 }
 eval($aliases_stub);
+
+/**
+ * Production alias
+ * Set each option to match your configuration
+ */
+$aliases['prod'] = array (
+  // This is the full site alias name from which we inherit its config.
+  'uri' => 'sl85.web.hostpoint.ch',
+  'root' => '/www/kulturpunkt-flawil.ch',
+  'remote-user' => 'olivie62',
+  'remote-host' => 'p1.server.hostpoint.ch',
+);
